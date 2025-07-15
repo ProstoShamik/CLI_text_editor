64fa36cfd6cbd9818079e51636a9eed0f0fcf82d
eidillum <zakiyev.shamil@outlook.com>
2025-07-15 11:45:04 +0300

    second cmmit

---
+++ src/editor.rs
@@ -0,0 +1,38 @@
+use crossterm::terminal::{enable_raw_mode, disable_raw_mode};
+use crossterm::event::{read, Event::Key, KeyCode::Char};
+
+pub struct Editor {
+
+}
+
+impl Editor {
+    pub fn default() -> Self {
+        Editor{}
+    }
+
+    pub fn run(&self) {
+        enable_raw_mode().unwrap();
+    
+        loop {
+            match read() {
+                Ok(Key(event)) => {
+                    println!("{:?} \r", event);
+                    match event.code {
+                        Char(c) => {
+                            if c == '\r' {continue}
+                            if c == 'q' {
+                                break;
+                            }
+                        },
+                        _ => (),   
+                    }   
+                },
+                Ok(_) => (),
+                Err(err) => {
+                    println!("Error: {}", err)
+                }
+            }
+        }
+        disable_raw_mode().unwrap(); 
+    }
+}
